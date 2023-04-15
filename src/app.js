9b956deb67b441ee2acf3725f7ba8642ab8b1377
julian19028 <121776944+julian19028@users.noreply.github.com>
2023-04-15 02:35:36 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let first = ["the", "our", "your"];
+let second = ["highest", "best", "big", "little"];
+let third = ["house", "company", "flag", "planet"];
+let domain = [".com", ".net", ".gov", ".co"];
+
+console.log("Any string");
+for (let i = 0; i < first.length; i++) {
+  // console.log(first[i]);
+  for (let j = 0; j < second.length; j++) {
+    for (let m = 0; m < third.length; m++) {
+      for (let t = 0; t < third.length; t++) {
+        console.log(first[i] + second[j] + third[m] + domain[t]);
+      }
+    }
+  }
+}
